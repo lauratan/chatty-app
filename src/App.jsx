c1a658d91d8b20b01841a8f43aff2bd40d2389a1
lauratan <laura.tan108@gmail.com>
2018-07-11 15:53:00 +0000

    Added and connected to websocket server

--- src/App.jsx
+++ src/App.jsx
@@ -27,16 +27,27 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    setTimeout(() => {
-      // Add a new message to the list of messages in the data store
-      const newMessage = {id: 3, username: 'Michelle', content: 'Hello there!'};
-      const messages = this.state.messages.concat(newMessage)
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({messages: messages})
-    }, 3000);
+    // setTimeout(() => {
+    //   // Add a new message to the list of messages in the data store
+    //   const newMessage = {id: 3, username: 'Michelle', content: 'Hello there!'};
+    //   const messages = this.state.messages.concat(newMessage)
+    //   // Update the state of the app component.
+    //   // Calling setState will trigger a call to render() in App and all child components.
+    //   this.setState({messages: messages})
+    // }, 3000);
+    this.socket = new WebSocket('ws:localhost:3001');
+    console.log('Connected to server')
+
   }
   
+  generateRandomString() {
+    let randomString = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < 6; i++)
+      randomString += possible.charAt(Math.floor(Math.random() * possible.length));
+    
+    return randomString;
+  }
   // onEnter(event) {
   //   const newMessage = [{username: this.state.currentUser.name , content: event.target.value}];
   //   this.setState((prevState) => {
@@ -45,7 +56,7 @@ export default class App extends Component {
   // }
 
   onEnter(event) {
-    const newMessage = [{username: this.state.currentUser.name , content: event.target.value}];
+    const newMessage = [{id: this.generateRandomString(), username: this.state.currentUser.name , content: event.target.value}];
     const messages = this.state.messages.concat(newMessage)
     if (event.key === 'Enter'){
       this.setState({messages: messages} , event.target.value='')
